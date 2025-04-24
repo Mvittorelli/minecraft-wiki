8653014e3314e7e142d6062d9830e38d5ba7f0ed
Mvittorelli <mvittorelli@edenschool.fr>
2025-04-24 15:41:21 +0200

    A

--- js/details.js
+++ js/details.js
@@ -1,13 +1,42 @@
-fetch("http://51.38.232.174:3000/v1/arena/entities", {
-  headers: {
-    "Content-type": "application/json",
-  },
-  method: "POST",
-  body: JSON.stringify({
-    entityId: 10,
-    x: 15,
-    z: -8,
-  }),
-})
-  .then((e) => e.json())
-  .then((json) => console.log(json));
+const API_URL = "http://51.38.232.174:3000/v1";
+const mob = await (
+  await fetch(
+    `${API_URL}/entities/${new URLSearchParams(location.search).get("id")}`
+  )
+).json();
+
+const desc = document.querySelector(".desc p");
+const name = document.querySelector("h2");
+const titleName = document.querySelector("#title-name");
+const titleIcon = document.querySelector("#title-icon");
+const mobImg = document.querySelector(".mob-img");
+const health = document.querySelector("#health");
+const armor = document.querySelector("#armor");
+const damage = document.querySelector("#damage");
+const classification = document.querySelector("#classification");
+const categoryClassification = document.querySelector(
+  "#category-classification"
+);
+const behavior = document.querySelector("#behavior");
+const width = document.querySelector("#width");
+const height = document.querySelector("#height");
+
+desc.innerHTML = mob.description;
+name.innerHTML = mob.name;
+titleName.innerHTML = mob.name;
+titleIcon.src = mob.icon;
+mobImg.src = mob.image;
+
+health.innerHTML = mob.health;
+armor.innerHTML = mob.armor;
+damage.innerHTML = mob.strength;
+classification.innerHTML = mob.classification;
+categoryClassification.innerHTML = `${mob.classification}&nbsp;`;
+behavior.innerHTML = mob.type;
+width.innerHTML = mob.width;
+height.innerHTML = mob.height;
+
+document.querySelector(".card").classList.add(mob.type);
+document.querySelector(".btn").classList.add(mob.type);
+
+for (let hr of document.querySelectorAll("hr")) hr.classList.add(mob.type);
--- js/home.js
+++ js/home.js
@@ -8,65 +8,61 @@ mobSearch.addEventListener("input", () => {
 });
 
 async function searchMob(value) {
-  const response = await fetch(`${API_URL}/entities?${value}`);
+  result.innerHTML = "";
+  const response = await fetch(`${API_URL}/entities?name=${encodeURIComponent(value)}`);
   const json = await response.json();
   for (let elements of json) {
-    console.log(elements);
-    result.appendChild(createCard(elements)); 
+    result.appendChild(createCard(elements));
   }
 }
 
 function createCard(elements) {
-  result.appendChild(
-    Object.assign(document.createElement("div"), {
-      innerHTML: elements.name,
-      className: `box ${elements.type}`,
-    })
-  );
-  console.log(result);
-  result.appendChild(
-    Object.assign(document.createElement("img"), { src: elements.image })
-  );
-  const info = Object.assign(document.createElement("div"), {
-    classList: "info",
+  const container = document.createElement("div");
+
+  const box = Object.assign(document.createElement("div"), {
+    innerHTML: elements.name,
+    className: `box ${elements.type}`,
   });
+
+  const image = Object.assign(document.createElement("img"), {
+    src: elements.image,
+  });
+
+  const info = Object.assign(document.createElement("div"), {
+    className: "info",
+  });
+
   info.appendChild(
     Object.assign(document.createElement("p"), {
       innerHTML: elements.classification,
     })
   );
   info.appendChild(
-    Object.assign(document.createElement("p"), { innerHTML: elements.type })
+    Object.assign(document.createElement("p"), {
+      innerHTML: elements.type,
+    })
   );
 
   const idk = document.createElement("div");
   idk.appendChild(info);
   idk.appendChild(document.createElement("hr"));
+
   const seemore = Object.assign(document.createElement("div"), {
     className: `box ${elements.type}`,
   });
+
   seemore.appendChild(
     Object.assign(document.createElement("a"), {
       href: `../html/details.html?id=${elements.id}`,
       innerHTML: "SEE MORE",
     })
   );
+
   idk.appendChild(seemore);
-  result.appendChild(idk);
 
-  return result;
+  container.appendChild(box);
+  container.appendChild(image);
+  container.appendChild(idk);
+
+  return container;
 }
-
-fetch("http://51.38.232.174:3000/v1/arena/entities", {
-  headers: {
-    "Content-type": "application/json",
-  },
-  method: "POST",
-  body: JSON.stringify({
-    entityId: 10,
-    x: 0,
-    z: 0,
-  }),
-})
-  .then((e) => e.json())
-  .then((json) => console.log(json));
